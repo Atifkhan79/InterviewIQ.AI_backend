0c8b5eecce3b1c8048150b670c49844901424773
Atifkhan79 <atif79918@gmail.com>
2026-07-04 19:36:12 +0500

    fix: properly configure CORS allowed origins

--- app.js
+++ app.js
@@ -20,9 +20,20 @@ import webhookRoutes from "./routes/webhookRoutes.js";
 export const app = express();
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: "http://localhost:5173 || process.env.FRONTEND_URL",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );

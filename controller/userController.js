f05575443ad30b21cc4cc911904072595b2569b0
Atifkhan79 <atif79918@gmail.com>
2026-07-05 11:54:53 +0500

    All bugs solves now its ready to use

--- controller/userController.js
+++ controller/userController.js
@@ -182,7 +182,7 @@ export const forgotPassword = AsyncHandler(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 4️⃣ Create reset URL (Moved safely AFTER token generation with hardcoded fallback string)
-  const baseUrl = process.env.FRONTEND_URL || frontedUrl || "http://localhost:5173";
+  const baseUrl = process.env.FRONTEND_URL || frontedUrl || "https://onlineinterviewmanagementsystem.netlify.app/";
   const resetPasswordUrl = `${baseUrl}/password/reset/${resetToken}`;
 
   const message = generateEmailTemplate(resetPasswordUrl);
